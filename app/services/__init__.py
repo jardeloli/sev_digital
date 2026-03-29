9d6653622fa85db079b8e88620376da777fc12e5
jardeloli <jardelsantosa380@gmail.com>
2026-03-29 17:19:15 -0300

    feat: adiciona __init__.py com exports dos services

---
+++ app/services/__init__.py
@@ -0,0 +1,7 @@
+from .auth_service import AuthService
+from .perfil_service import PerfilService
+from .permissao_service import PermissaoService
+from .servico_service import ServicoService
+from .servidor_service import ServidorService
+from .veiculo_service import VeiculoService
+from .registroSev_service import RegistroSevService
