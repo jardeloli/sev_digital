d4c9a7dc09c5853b80035d11397e3290fab4720c
jardeloli <jardelsantosa380@gmail.com>
2026-03-29 15:46:13 -0300

    feat: cria service de Servidor

---
+++ app/services/servidor_service.py
@@ -0,0 +1,39 @@
+from django.core.exceptions import ValidationError
+from ..models import Servidor, Perfil
+
+class ServidorService:
+    @staticmethod
+    def criar_servidor(siape, nome, cpf, data_nascimento, email, senha, id_perfil):
+        
+        if Servidor.objects.filter(cpf=cpf).exists():
+            raise ValidationError("CPF já existe.")
+    
+        if Servidor.objects.filter(email=email).exists():
+            raise ValidationError("Email já existe.")
+        
+        perfil = Perfil.objects.get(pk=id_perfil)
+
+        servidor = Servidor(
+            siape = siape,
+            nome_servidor = nome,
+            cpf = cpf,
+            data_nascimento = data_nascimento,
+            email = email,
+            senha = senha,
+            id_perfil = perfil
+        )
+
+        servidor.save()
+
+        return servidor
+    
+    @staticmethod
+    def deletar_servidor(siape):
+       if not Servidor.objects.filter(pk=siape).exists():
+            raise ValidationError("Servidor não encontrado.")
+       
+       Servidor.objects.filter(pk=siape).delete()
+
+       return "Servidor deletado com sucesso."
+    
+    
