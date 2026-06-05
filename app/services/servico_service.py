eaf0ae1d4f4767be4ce22e45e7353c87011fd172
jardeloli <jardelsantosa380@gmail.com>
2026-06-05 01:09:42 -0300

    feature: adiciona os campos recém criados na model nas funções da classe Serviço

--- app/services/servico_service.py
+++ app/services/servico_service.py
@@ -1,28 +1,50 @@
 from django.core.exceptions import ValidationError
-from ..models import Servico
+from ..models import Servico, RegistroSev
 
 class ServicoService:
     @staticmethod
-    def criar_servico(nome_servico, descricao=''):
+    def criar_servico(nome_servico, descricao='', categoria='OUTROS',
+    status='ATIVO'):
 
         if Servico.objects.filter(nome_servico=nome_servico).exists():
             raise ValidationError(f"Serviço com nome '{nome_servico}' já existe.")
         
         servico = Servico.objects.create(
             nome_servico = nome_servico,
-            descricao = descricao
+            descricao = descricao,
+            categoria=categoria,
+            status=status
         )
 
         servico.save()
         return servico
     
-    def deletar_servico(nome_servico):
-        if not Servico.objects.filter(nome_servico=nome_servico).exists():
-            raise ValidationError(f"Serviço com nome '{nome_servico}' não existe.")
-        
-        Servico.objects.get(nome_servico=nome_servico).delete()
+    @staticmethod
+    def deletar_servico(id_servico):
 
-        return "Serviço deletado com sucesso."
+        try:
+
+            servico = Servico.objects.get(
+                pk=id_servico
+            )
+
+        except Servico.DoesNotExist:
+
+            raise ValidationError(
+                "Serviço não encontrado."
+            )
+
+        if RegistroSev.objects.filter(
+            servico=servico
+        ).exists():
+
+            raise ValidationError(
+                "Não é possível excluir este serviço porque ele possui registros vinculados."
+            )
+
+        servico.delete()
+
+        return "Serviço excluído com sucesso."
     
     def listar_servicos():
         return Servico.objects.all()
